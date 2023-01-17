5afa58e5a4ed71cc675fc98b4b75a8bb374e9b1f
eric-lab-star <cyon256@icloud.com>
2023-01-17 16:55:50 +0900

    morestring test

--- go.mod
+++ go.mod
@@ -1,2 +1,5 @@
 module github.com/Eric-lab-star/go-hello
+
 go 1.19
+
+require github.com/google/go-cmp v0.5.9
--- hello.go
+++ hello.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/Eric-lab-star/go-hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	r := morestrings.ReverseRunes("Hello, world")
 	fmt.Println(r)
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
